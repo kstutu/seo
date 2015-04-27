140bcedf9873b30bbb2bc2274c3ce17f7966e770
Kstart <zhaonan.zhang@kpmg.com>
2015-04-27 12:16:41 +0800

    some change

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,6 +4,15 @@ use Think\Controller;
 class IndexController extends Controller {
 	public function index(){
 		$this->title = C('APP_TITLE');
+		$data = M('article')->select();
+		$this->data = $data;
+		$this->display('home');
+	}
+
+	public function detail(){
+		$id = I('id');
+		$data = M('article')->where(array('id'=>$id))->find();
+		$this->data = $data;
 		$this->display();
 	}
 
